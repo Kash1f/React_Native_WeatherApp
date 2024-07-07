424396c68ab3c7f0b9fc3d4626f40c4bead85007
Kashif <ahmed.kashif1001@gmail.com>
2024-07-07 23:47:59 +0500

    navigation, nativewind

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,27 +1,24 @@
-import { View, Text, ImageBackground, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  Dimensions,
+  TextInput,
+} from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { theme } from "../theme";
 
 export default function HomeScreen() {
   return (
-
-    //background image for the app
-    <View style={{ flex: 1, position: "relative" }}>
-      <StatusBar style="light" />
-      <Image
-        blurRadius={70}
-        style={{ height: "100%", width: Dimensions.get("screen").width * 0.99999 }}
-        source={require("../assets/images/bg.png")}/>
-    
-
-
-    <SafeAreaView style={{display:'flex', flex:1}}>
-
-    </SafeAreaView>
-
+    <View className="flex-1">
+      <Text>Hello</Text>
     </View>
+  )
+   
 
-  );
+
 }
+
